bea0f692c625c21541e7346344f84b0fe1152653
joakinve <jokinve1997@gmail.com>
2023-05-22 15:01:15 -0300

    Arreglo boludo

--- src/app/shared/modules/index.ts
+++ src/app/shared/modules/index.ts
@@ -1,2 +1,2 @@
-export * from './material/material.module'; // TIene que ir primero, sino genera error undefined ya que carga despues de los elementos que lo deben importar.
+export * from './material/material.module'; // Tiene que ir primero, sino genera error undefined ya que carga despues de los elementos que lo deben importar.
 export * from './components/components.module';
