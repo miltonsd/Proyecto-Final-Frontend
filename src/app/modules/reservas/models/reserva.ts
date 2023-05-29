7da862155d59f962fec22af9fa946fa2b51ec967
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2023-05-29 17:51:33 -0300

    Validaciones en el formulario de Reservas

---
+++ src/app/modules/reservas/models/reserva.ts
@@ -0,0 +1,68 @@
+export interface Rol {
+  id_rol: number
+  descripcion: string
+  Categoria: number
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+}
+
+export interface Categoria {
+  id_categoria: number
+  descripcion: string
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+}
+
+export interface Usuario {
+  id_usuario: number
+  nombre: string
+  apellido: string
+  email: string
+  isConfirmado: boolean
+  documento: string
+  direccion: string
+  telefono: string
+  fechaNacimiento: string
+  Rol: Rol
+  Categoria: Categoria
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+}
+
+export interface Mesa {
+  id_mesa: number
+  capacidad: number
+  ubicacion: string
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+}
+
+export interface Reservas {
+  id_reserva: number
+  //   fechaHora: Date
+  fechaHora: string
+  cant_personas: number
+  isPendiente: boolean
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+  Usuario: Usuario
+  Mesa: Mesa
+}
+
+export interface Reserva {
+  id_reserva: number
+  //   fechaHora: Date
+  fechaHora: string
+  cant_personas: number
+  isPendiente: boolean
+  id_usuario: number
+  id_mesa: number
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+}
--- src/app/modules/reservas/reservas.component.ts
+++ src/app/modules/reservas/reservas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output } from '@angular/core'
 import * as moment from 'moment'
 import 'moment/locale/es'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
@@ -6,6 +6,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ReservasService } from '@pa/reservas/services'
 import { MesasService } from '@pa/mesas/services'
 import { IMesa, TableColumn } from '@pa/shared/models'
+import { Reserva } from '@pa/reservas/models'
+import { map } from 'rxjs/operators'
 
 moment.locale('es')
 
@@ -15,47 +17,40 @@ moment.locale('es')
   styleUrls: ['./reservas.component.css']
 })
 export class ReservasComponent implements OnInit {
+  @Output() fechaHora!: string
+  @Output() cantidad!: number
   horas = ['18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
-  mesas: IMesa[] = [
-    // { id: 1, available: true },
-    // { id: 2, available: false },
-    // { id: 3, available: true },
-    // { id: 4, available: true },
-    // { id: 5, available: false },
-    // { id: 6, available: true },
-    // { id: 7, available: false },
-    // { id: 8, available: true },
-    // { id: 9, available: true },
-    // { id: 10, available: false },
-    // { id: 11, available: true },
-    // { id: 12, available: true },
-    // { id: 13, available: true },
-    // { id: 14, available: false },
-    // { id: 15, available: true },
-    // { id: 16, available: false },
-    // { id: 17, available: true },
-    // { id: 18, available: true },
-    // { id: 19, available: false },
-    // { id: 20, available: true },
-    // { id: 21, available: false },
-    // { id: 22, available: false }
-  ]
+  mesas: IMesa[] = []
   minDate: Date
   maxDate: Date
-  displayedColumns: string[] = ['fechaHora', 'cant_personas', 'id_mesa']
   mostrarReservas = false
   respuesta: any
-  reservasDatos: any[] = []
+  reservas: Reserva[] = []
+
+  // Formulario de reservas
   formulario = new FormGroup({
-    fecha: new FormControl('', { validators: [Validators.required] }),
-    hora: new FormControl('', { validators: [Validators.required] }),
-    cantidad: new FormControl('', {
-      validators: [Validators.required, Validators.min(1), Validators.max(6)]
+    fechaHoraCantidad: new FormGroup({
+      fecha: new FormControl('', { validators: [Validators.required] }),
+      hora: new FormControl('', { validators: [Validators.required] }),
+      cantidad: new FormControl(1, {
+        validators: [Validators.required, Validators.min(1), Validators.max(6)]
+      })
     }),
-    mesa: new FormControl('', { validators: [Validators.required] })
+    mesa: new FormControl(0, {
+      validators: [Validators.required, Validators.min(1)] // Valida que el id recibido no sea 0
+    })
   })
 
-  reservasColumnas: TableColumn[] = []
+  // Defino las columnas de la tabla de reservas
+  columnas: TableColumn[] = [
+    { name: 'Fecha y hora', dataKey: 'fechaHora' },
+    {
+      name: 'Cantidad de personas',
+      dataKey: 'cant_personas',
+      isSortable: true
+    },
+    { name: 'Mesa', dataKey: 'id_mesa' }
+  ]
 
   constructor(
     private _reservasService: ReservasService,
@@ -67,98 +62,107 @@ export class ReservasComponent implements OnInit {
     const currentDate = new Date().getDate()
     this.minDate = new Date(currentYear, currentMonth, currentDate)
     this.maxDate = new Date(currentYear, currentMonth + 2, 0)
-    //TODO: Aca nos tendriamos que traer las mesas para el horario seleccionado asi se ven las disponibles y no disp.
-    this._mesasService.getAllMesas().subscribe({
-      next: (respuesta: any) => {
-        const listaMesas: IMesa[] = []
-        respuesta.forEach((mesa: IMesa) => {
-          listaMesas.push({
-            id_mesa: mesa.id_mesa,
-            capacidad: mesa.capacidad,
-            ubicacion: mesa.ubicacion,
-            createdAt: mesa.createdAt,
-            updatedAt: mesa.updatedAt,
-            available: true
-          })
-        })
-        this.mesas = listaMesas
-        console.log(this.mesas)
-      },
-      error: (err) => {
-        console.error(`Código de error ${err.status}: `, err.error.msg)
-      }
-    })
   }
 
   ngOnInit(): void {
-    console.log(this.formulario.controls.fecha.value)
-    console.log(this.formulario.controls.hora.value)
-    // Defino las columnas de la tabla de reservas
-    /*
-    export interface TableColumn {
-    name: string; // Nombre de la columna
-    dataKey: string; // Nombre de la key del dato actual en esta columna
-    isImage?: boolean; // La columna muestra una imagen?
-    showDetailsButton?: boolean; // La columna tiene un boton para ver detalles?
-    editButton?: boolean; // La columna tiene un boton para editar?
-    deleteButton?: boolean; // La columna tiene un boton para eliminar?
-    detailsUrl?: string;
-    editUrl?: string;
-    isSortable?: boolean; // Se puede ordenar la columna?
-    }
-    */
-    this.reservasColumnas = [
-      { name: 'Fecha y hora', dataKey: 'fechaHora' },
-      {
-        name: 'Cantidad de personas',
-        dataKey: 'cant_personas',
-        isSortable: true
-      },
-      { name: 'Mesa', dataKey: 'id_mesa' }
-    ]
     // Busca las reservas
-    this._reservasService.getAllReservas().subscribe({
-      next: (response: any) => {
-        this.respuesta = response
-        const listaReservas: any[] = []
-        this.respuesta.forEach((reserva: any) => {
-          if (reserva.isPendiente) {
-            listaReservas.push({
-              id_reserva: reserva.id_reserva,
-              fechaHora: moment(reserva.fechaHora).format('DD/MM/yyyy HH:mm'),
-              cant_personas: reserva.cant_personas,
-              isPendiente: reserva.isPendiente,
-              id_usuario: reserva.Usuario.id_usuario,
-              id_mesa: reserva.Mesa.id_mesa,
-              createdAt: reserva.createdAt,
-              updatedAt: reserva.updatedAt
-            })
-          }
+    this.getAllReservas() // Busca las reservas pendientes del usuario
+    this.getAllMesas() // Busca las mesas para el formulario
+    // Controla si hubo cambios en el input de hora
+    this.formulario
+      .get('fechaHoraCantidad')
+      ?.valueChanges.subscribe((valor) => {
+        if (valor.cantidad != 0) {
+          this.cantidad = valor.cantidad || 1
+          const fecha = moment(valor.fecha).format('DD/MM/yyyy')
+          this.fechaHora = fecha + ' ' + valor.hora
+          // Filtra las reservas pendientes por la fecha y hora ingresadas
+          const reservasFiltradas = this.reservas.filter(
+            (r) => r.fechaHora === this.fechaHora
+          )
+          this.mesas.forEach((mesa) => {
+            if (mesa.capacidad < this.cantidad) {
+              mesa.available = false
+            } else {
+              mesa.available = true
+            }
+          })
+          reservasFiltradas.forEach((reserva) => {
+            // Si existen reservas para esa fecha y hora, asigna las mesas correpondientes como ocupadas
+            const posMesa = reserva.id_mesa - 1
+            this.mesas[posMesa].available = false
+          })
+        }
+      })
+  }
+
+  getAllReservas() {
+    // Se obtiene el listado de reservas pendientes del usuario
+    this._reservasService
+      .getAllReservas()
+      .pipe(
+        map((res: any) => {
+          this.reservas = Object.keys(res)
+            .map((r) => ({
+              id_reserva: res[r].id_reserva,
+              fechaHora: moment(res[r].fechaHora).format('DD/MM/yyyy HH:mm'),
+              cant_personas: res[r].cant_personas,
+              isPendiente: res[r].isPendiente,
+              id_usuario: res[r].Usuario.id_usuario,
+              id_mesa: res[r].Mesa.id_mesa
+            }))
+            .filter((r) => r.isPendiente)
+            .sort((a, b) => a.fechaHora.localeCompare(b.fechaHora))
         })
-        this.reservasDatos = listaReservas
-      },
-      error: (err) => {
-        console.error(`Código de error ${err.status}: `, err.error.msg)
-      }
+      )
+      .subscribe({
+        error: (err: any) =>
+          console.error(`Código de error ${err.status}: `, err.error.msg)
+      })
+  }
+
+  getAllMesas() {
+    //TODO: Aca nos tendriamos que traer las mesas para el horario seleccionado asi se ven las disponibles y no disp.
+    this._mesasService
+      .getAllMesas()
+      .pipe(
+        map((res: any) => {
+          this.mesas = Object.keys(res).map((m) => ({
+            id_mesa: res[m].id_mesa,
+            capacidad: res[m].capacidad,
+            ubicacion: res[m].ubicacion,
+            available: true
+          }))
+        })
+      )
+      .subscribe({
+        error: (err: any) =>
+          console.error(`Código de error ${err.status}: `, err.error.msg)
+      })
+  }
+
+  onSelectMesa(eventData: { id: number }) {
+    // Asigna el id de la mesa al control del formulario
+    // Sirve para actualizar los valores de los controles del formulario
+    this.formulario.patchValue({
+      mesa: eventData.id
     })
   }
 
   onVerReservas() {
-    if (this.mostrarReservas) {
-      this.mostrarReservas = false
-    } else {
-      this.mostrarReservas = true
-    }
+    this.mostrarReservas = !this.mostrarReservas
   }
 
   onSubmit() {
     if (this.formulario.valid) {
-      const fecha = moment(this.formulario.value.fecha).format('yyyy-MM-DD')
-      const fechaHora = fecha + ' ' + this.formulario.value.hora
-      console.log(fechaHora)
+      const fecha = moment(
+        this.formulario.value.fechaHoraCantidad?.fecha
+      ).format('yyyy-MM-DD')
+      const fechaHora =
+        fecha + ' ' + this.formulario.value.fechaHoraCantidad?.hora
       const reserva = {
         fechaHora: fechaHora,
-        cant_personas: this.formulario.value.cantidad,
+        cant_personas: this.formulario.value.fechaHoraCantidad?.cantidad,
         id_usuario: 1,
         id_mesa: this.formulario.value.mesa
       }
@@ -171,6 +175,8 @@ export class ReservasComponent implements OnInit {
           alert(err.msg)
         }
       })
+      console.log('Reserva: ')
+      console.log(reserva)
     } else {
       this.formulario.markAllAsTouched()
     }
