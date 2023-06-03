3a71848166f6227bc188b44ed7885a1f0b8cfe2d
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2023-06-02 22:03:12 -0300

    Configurada la conexion al backend desde cualquier dispositivo de la red

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  // apiUrl: 'http://localhost:3000' // El backend solo funciona en la computadora que lo está corriendo
+  apiUrl: 'http://' + window.location.hostname + ':3000' // Permite el funcionamiento del backend desde localhost y cualquier dispositivo en red
 }
