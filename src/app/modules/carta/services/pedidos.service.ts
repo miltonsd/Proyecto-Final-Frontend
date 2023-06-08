f5b65085f985bbef9dffa69ac84242ab0465d53c
joakinve <jokinve1997@gmail.com>
2023-06-08 15:11:07 -0300

    Service de pedidos

---
+++ src/app/modules/carta/services/pedidos.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidosService {
+  url = environment.apiUrl + '/pedidos'
+
+  constructor(private _http: HttpClient) {}
+
+  getAllPedidos() {
+    return this._http.get(`${this.url}/`)
+  }
+
+  getOnePedido(id_pedido: number) {
+    return this._http.get(`${this.url}/${id_pedido}`)
+  }
+
+  createPedido(pedido: any) {
+    return this._http.post(`${this.url}/create`, pedido)
+  }
+
+  updatePedido(id_pedido: number, pedido: any) {
+    return this._http.patch(`${this.url}/${id_pedido}`, pedido)
+  }
+
+  deletePedido(id_pedido: number) {
+    return this._http.delete(`${this.url}/${id_pedido}`)
+  }
+}
