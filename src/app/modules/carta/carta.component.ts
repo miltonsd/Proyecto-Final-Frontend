98579a29f214f1e938bea0896de9fbc63609a968
Juan Frontons <juan.f@jackrabbitmobile.com>
2023-06-11 15:08:32 -0300

    agregue una animacion medio pelo

--- src/app/modules/carta/carta.component.ts
+++ src/app/modules/carta/carta.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core'
 import { CartaService } from './services/carta.service'
+import { trigger, transition, animate, style } from '@angular/animations'
 
 import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'pa-carta',
   templateUrl: './carta.component.html',
-  styleUrls: ['./carta.component.css']
+  styleUrls: ['./carta.component.css'],
+  animations: [
+    trigger('slide', [
+      transition(':enter', [
+        style({ transform: 'translateY(-150%)', opacity: '0%' }),
+        animate(
+          '0.5s 500ms ease-in-out',
+          style({ transform: 'translateY(0%)', opacity: '100%' })
+        )
+      ]),
+      transition(':leave', [
+        animate(
+          '0.5s 500ms ease-in-out',
+          style({ transform: 'translateY(-150%)', opacity: '0%' })
+        )
+      ])
+    ])
+  ]
 })
 export class CartaComponent implements OnInit {
   tiposP: any[] = []
