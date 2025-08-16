56e5a421acd63f7aa66a58402a82eb187375971c
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2025-08-16 18:46:46 -0300

    Manuales de usuario

--- src/app/modules/home/views/faq/faq.component.ts
+++ src/app/modules/home/views/faq/faq.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from '@pa/shared/services/auth.service'
 
 @Component({
   selector: 'pa-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent {
+export class FaqComponent implements OnInit {
+  constructor(private _authService: AuthService) {}
   // Listado de preguntas y respuestas frecuentes
+  manualUrl = ''
   faqs = [
     {
       q: '¿Existe algún requerimiento para visualizar la carta de productos?',
@@ -53,6 +56,39 @@ export class FaqComponent {
       a: `Una mesa está habilitada si no está asignada a ningún usuario, por lo tanto cuando sí está asignada a uno, la mesa se 
           encuentra en estado deshabilitada. Esta gestión del estado de las mesas se realiza de manera automática por parte del 
           sistema pero a su vez los mozos pueden gestionar dicho estado de manera manual por cualquier inconveniente que haya.`
+    },
+    {
+      q: 'Necesito ayuda con el sistema.',
+      a: ``
     }
   ]
+
+  ngOnInit(): void {
+    let rolUsuario: number // Almacena el rol del usuario logueado para mostrar el manual correspondiente
+    if (this._authService.loggedIn()) {
+      rolUsuario = this._authService.getRol() // Asigna rol de usuario
+    } else {
+      rolUsuario = 2 // Por defecto si no hay rol pone el de cliente (Id = 2)
+    }
+    switch (rolUsuario) {
+      case 1: // Administrador
+        this.manualUrl =
+          'https://drive.google.com/file/d/1dVjn4zJqBRzI6T_Bo4ClHDkwIlt2OMxR/view?usp=drive_link'
+        break
+      case 3: // Mozo
+        this.manualUrl =
+          'https://drive.google.com/file/d/17rb9za1wPz5FBUiEPRsUvm2pB1PEOwT3/view?usp=drive_link'
+        break
+      case 4: // Cocina
+        this.manualUrl =
+          'https://drive.google.com/file/d/1oZ5MH0PRyafhe6I7bmX2cwGTVcrEZH_0/view?usp=drive_link'
+        break
+      default: // Cliente o Usuario no logueado
+        this.manualUrl =
+          'https://drive.google.com/file/d/1nFZxX9zUFetkvnMi4JF-U6qeoQnJOkYT/view?usp=drive_link'
+        break
+    }
+
+    this.faqs[8].a = `Revisa el manual de usuario. <a href="${this.manualUrl}" target="_blank">Ver manual</a>`
+  }
 }
